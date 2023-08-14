e0d1c926f73558ec17952314dfb668250e308668
naser1 <naser1@uni-bremen.de>
2023-08-14 20:34:37 +0200

    change get_path_blender_scene method

--- src/utils/path_utils.py
+++ src/utils/path_utils.py
@@ -2,20 +2,25 @@ from pathlib import Path
 
 
 def get_project_root():
+    p = get_suturo_blenderproc_path()
+    return p.parent
+
+
+def get_suturo_blenderproc_path():
     """Return the root directory of the project."""
     p = get_project_src_dir()
     return p.parent
 
 
 def get_path_id2name_json():
-    p = Path.cwd().joinpath(get_project_root(), "data", "id2name.json")
+    p = Path.cwd().joinpath(get_suturo_blenderproc_path(), "data", "id2name.json")
     if not p.exists():
         raise Exception("id2name.json doesn't exist")
     return p
 
 
 def get_path_yaml_config(filename):
-    p = Path.cwd().joinpath(get_project_root(), "data/yaml", filename)
+    p = Path.cwd().joinpath(get_suturo_blenderproc_path(), "data/yaml", filename)
     if p.suffix != ".yaml":
         p = p.with_suffix(".yaml")
         if p.is_file():
@@ -28,13 +33,6 @@ def get_path_yaml_config(filename):
     return p
 
 
-def get_path_blender_scene(path_to_project: str, blend_scene: str):
-    p = Path(path_to_project).joinpath("scenes", blend_scene)
-    if p.suffix != ".blend":
-        raise Exception(f"Expected .blend file, got {p.suffix}")
-    return p
-
-
 def get_project_src_dir():
     p = Path.cwd()
     while p.stem != "src":
@@ -44,7 +42,16 @@ def get_project_src_dir():
 
 
 def get_path_output_dir():
-    p = Path.cwd().joinpath(get_project_root(), "output")
+    p = Path.cwd().joinpath(get_suturo_blenderproc_path(), "output")
     if not p.exists():
         Path(p).mkdir(parents=True)
+    return p
+
+
+def get_path_blender_scene(scene):
+    p = Path.cwd().joinpath(get_project_root(), "blender_data/scenes", scene)
+    if not p.exists():
+        print("Doesn't exist")
+        return Path.cwd().joinpath(get_project_root(), "blender_data/scenes", f"{scene}.blend")
+
     return p
