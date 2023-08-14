4ccd5e6d3f3e335241e61d329a0ef5281f70d956
naser1 <naser1@uni-bremen.de>
2023-08-14 20:16:02 +0200

    add several utilities and toy config

---
+++ src/utils/path_utils.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+
+
+def get_project_root():
+    """Return the root directory of the project."""
+    p = get_project_src_dir()
+    return p.parent
+
+
+def get_path_id2name_json():
+    p = Path.cwd().joinpath(get_project_root(), "data", "id2name.json")
+    if not p.exists():
+        raise Exception("id2name.json doesn't exist")
+    return p
+
+
+def get_path_yaml_config(filename):
+    p = Path.cwd().joinpath(get_project_root(), "data/yaml", filename)
+    if p.suffix != ".yaml":
+        p = p.with_suffix(".yaml")
+        if p.is_file():
+            return p
+        else:
+            raise Exception("File doesn't exist")
+
+    if not p.is_file():
+        raise Exception("File doesn't exist")
+    return p
+
+
+def get_path_blender_scene(path_to_project: str, blend_scene: str):
+    p = Path(path_to_project).joinpath("scenes", blend_scene)
+    if p.suffix != ".blend":
+        raise Exception(f"Expected .blend file, got {p.suffix}")
+    return p
+
+
+def get_project_src_dir():
+    p = Path.cwd()
+    while p.stem != "src":
+        p = p.parent
+
+    return p
+
+
+def get_path_output_dir():
+    p = Path.cwd().joinpath(get_project_root(), "output")
+    if not p.exists():
+        Path(p).mkdir(parents=True)
+    return p
---
+++ src/utils/yaml_config.py
@@ -0,0 +1,29 @@
+import yaml
+from utils import path_utils
+
+
+class YAMLConfig(object):
+    def __init__(self, filename):
+        config = str(path_utils.get_path_yaml_config(filename))
+        with open(config, 'r') as stream:
+            data_loaded = yaml.safe_load(stream)
+        self._data = data_loaded
+
+    def print(self):
+        print(self._data)
+
+    def get_list_object_positions(self):
+        pos_dict = self._data['object_positions']
+        return [v for k, v in pos_dict.items()]
+
+    def get_list_camera_positions(self):
+        locations_dict = self._data['camera_positions']
+        return [v for k, v in locations_dict.items()]
+
+    def get_position_of_interest(self):
+        return self._data['position_of_interest']
+
+    def get_objects(self):
+        return self._data['objects']
+
+
