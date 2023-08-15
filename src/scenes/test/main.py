e0f992d79e47540d2355ec96eb4b73a423a244a6
naser1 <naser1@uni-bremen.de>
2023-08-15 19:08:38 +0200

    add scene initialization to test pipeline

--- src/scenes/test/main.py
+++ src/scenes/test/main.py
@@ -1,6 +1,7 @@
 import blenderproc as bproc
 import sys
 from pathlib import Path
+import numpy as np
 
 p = Path.cwd()
 while p.stem != "src":
@@ -14,9 +15,16 @@ import utils.path_utils
 import suturo_blenderproc.scene_init
 
 args = scenes.argparser.get_argparse()
-objs = bproc.loader.load_blend(utils.path_utils.get_path_blender_scene(args.scene))
 config = utils.yaml_config.YAMLConfig(filename=args.config_yaml)
-scene_initializer = suturo_blenderproc.scene_init.SceneInitializer(yaml_config=config, mesh_objects=objs)
-for o in objs:
-    print(o.get_name())
+scene_initializer = suturo_blenderproc.scene_init.SceneInitializer(yaml_config=config)
+scene_initializer.initialize_scene()
+objs = scene_initializer.get_objects2annotate()
+tables = scene_initializer.get_table_surfaces_round()
+bproc.object.sample_poses(
+    objs,
+    sample_pose_func=scene_initializer.sample_object_positions_gaussian,
+    objects_to_check_collisions=objs,
+    max_tries=500
+)
+
 os.abort()
