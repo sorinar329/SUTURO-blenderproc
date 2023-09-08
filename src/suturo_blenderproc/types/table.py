073aea2b6c779e839ea1634fad34b90ffb080d5d
naser1 <naser1@uni-bremen.de>
2023-09-08 15:45:17 +0200

    refactor code, Table now consists of a table surface, chair and legs.

--- src/suturo_blenderproc/types/table.py
+++ src/suturo_blenderproc/types/table.py
@@ -5,24 +5,44 @@ import suturo_blenderproc.types.entity
 class Table(suturo_blenderproc.types.entity.Entity):
     def __init__(self):
         super().__init__()
+        self.table_surface = TableSurface()
+        self.table_legs = TableLegs()
+        self.table_chairs = TableChairs()
+
+    def get_mesh_objects_from_table(self):
+        mesh_objects = [self.table_chairs.mesh_object, self.table_legs.mesh_object, self.table_surface.mesh_object]
+        return mesh_objects
+
+
+class TableSurface(suturo_blenderproc.types.entity.Entity):
+    def __init__(self):
+        super().__init__()
         self.height = 0.0
 
 
-class RectangularTable(Table):
+class RectangularTableSurface(TableSurface):
     def __init__(self):
         super().__init__()
         self.x_size = 0.0
         self.y_size = 0.0
 
 
-class RoundTable(Table):
+class RoundTableSurface(TableSurface):
     def __init__(self):
         super().__init__()
         self.radius = 0.0
 
 
-class OvalTable(Table):
+class OvalTableSurface(TableSurface):
     def __init__(self):
         super().__init__()
         self.semi_major_x = 0.0
         self.semi_major_y = 0.0
+
+
+class TableLegs(suturo_blenderproc.types.entity.Entity):
+    pass
+
+
+class TableChairs(suturo_blenderproc.types.entity.Entity):
+    pass
