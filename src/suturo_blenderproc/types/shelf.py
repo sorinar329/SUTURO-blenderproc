c574edb599882f3148b0eb45f38f49bafa5ab48b
naser1 <naser1@uni-bremen.de>
2023-09-08 15:43:39 +0200

    add method add_shel_floor, add additional fields

--- src/suturo_blenderproc/types/shelf.py
+++ src/suturo_blenderproc/types/shelf.py
@@ -1,10 +1,5 @@
 import suturo_blenderproc.types.entity
-
-
-class Shelf(suturo_blenderproc.types.entity.Entity):
-    def __init__(self):
-        super().__init__()
-        self.shelf_floors = []
+import numpy as np
 
 
 class ShelfFloor(suturo_blenderproc.types.entity.Entity):
@@ -13,3 +8,14 @@ class ShelfFloor(suturo_blenderproc.types.entity.Entity):
         self.x_size = 0.0
         self.y_size = 0.0
         self.height = 0.0
+
+
+class Shelf(suturo_blenderproc.types.entity.Entity):
+    def __init__(self):
+        super().__init__()
+        self.shelf_floors = []
+        self.x_size = 0.0
+        self.y_size = 0.0
+
+    def add_shelf_floor(self, shelf_floor: ShelfFloor):
+        self.shelf_floors.append(shelf_floor)
