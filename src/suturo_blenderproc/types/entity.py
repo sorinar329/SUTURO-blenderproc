dff1fefd0003af7aef8ab5c4f35ff1bd8f6e75b6
naser1 <naser1@uni-bremen.de>
2023-09-26 12:12:59 +0200

    make attributes instance attr

--- src/suturo_blenderproc/types/entity.py
+++ src/suturo_blenderproc/types/entity.py
@@ -2,9 +2,7 @@ import numpy as np
 
 
 class Entity:
-    center = np.zeros(shape=(3, 1))
-    bbox = np.zeros(shape=(8, 3))
-    mesh_object = None
-
     def __init__(self):
-        self.id = ""
+        self.center = np.zeros(shape=(3, 1))
+        self.bbox = np.zeros(shape=(8, 3))
+        self.mesh_object = None
