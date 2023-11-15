76bbaf9b1206b3a06762c12f9d70f749245a5af4
naser1 <naser1@uni-bremen.de>
2023-11-15 14:57:52 +0100

    readjusted blenderproc pipeline. NOTE: Currently there is a bug with shelves.

--- src/scripts/blenderproc_pipeline/single_room/main.py
+++ src/scripts/blenderproc_pipeline/single_room/main.py
@@ -1,8 +1,5 @@
 import blenderproc as bproc
-import random
-import logging
 import sys
-import time
 from pathlib import Path
 
 p = Path.cwd()
@@ -10,54 +7,44 @@ while p.stem != "src":
     p = p.parent
 
 sys.path.append(str(p))
-import scenes.argparser
-from utils.logger import Logger
-import utils.yaml_config
-import utils.path_utils
+from suturo_blenderproc.utils.logger import Logger
+import suturo_blenderproc.utils.yaml_config as yaml_config
 import suturo_blenderproc.scene_init
-import suturo_blenderproc.sampler.pose_sampler
+from suturo_blenderproc.sampler.pose_sampler import CameraPoseSampler, LightPoseSampler, ObjectPoseSampler
 import suturo_blenderproc.types.shelf
+import scripts.argparser
 from suturo_blenderproc.sampler.object_partitions import ObjectPartition, PartitionType, validate_partitions
 import os
-from scripts.annotations.convert_to_yolo import create_yolo_dataset
-from utils.blenderproc_utils import hide_mesh_objects
+from yolo.annotations.convert_to_yolo import create_yolo_dataset
+from suturo_blenderproc.utils.blenderproc_utils import hide_mesh_objects, duplicate_objects
 import blenderproc.python.types.MeshObjectUtility
 import numpy as np
 
 # import scripts.model.yolov8 as yolo
 
-args = scenes.argparser.get_argparse()
-config = utils.yaml_config.YAMLConfig(filename=args.config_yaml)
+args = scripts.argparser.get_argparse()
+config = yaml_config.YAMLConfig(filename=args.config_yaml)
 
 
 def deploy_scene(x: int, scene_initializer: suturo_blenderproc.scene_init.SceneInitializer, logger: Logger):
-    hide_mesh_objects(scene_initializer.get_all_mesh_objects(), True)
+    hide_mesh_objects(scene_initializer.get_all_mesh_objects_id2name(), True)
+
     objects = scene_initializer.get_objects2annotate()
-    objects = utils.blenderproc_utils.duplicate_objects(objects, config)
-    hide_mesh_objects(objects, True)
+    objects = duplicate_objects(objects, config)
     scene_collection = scene_initializer.get_scene_collection()
-    furnitures = []
-    room = scene_collection.get("Room")[0]
-
-    furnitures.extend(room.get_mesh_objects_from_room())
     for table in scene_collection.get("Tables"):
-        furnitures.extend(table.get_mesh_objects_from_table())
-    for shelf in scene_collection.get("Shelves"):
-        furnitures.extend(shelf.get_mesh_objects_from_shelf())
+        hide_mesh_objects(table.get_mesh_objects_from_table(), False)
 
-    hide_mesh_objects(furnitures, False)
+    room = scene_collection.get("Room")[0]
     num_partitions = -(len(objects) // -4)
-    object_partitioner = suturo_blenderproc.sampler.object_partitions.ObjectPartition(num_partitions, objects)
+    object_partitioner = ObjectPartition(num_partitions=num_partitions, objects=objects)
+    partitions = object_partitioner.create_partition(PartitionType.LESS_PROBABLE_OBJECTS,
+                                                     probability_reduction_factor=0.5)
 
-    partitions = object_partitioner.create_partition(
-        suturo_blenderproc.sampler.object_partitions.PartitionType.LESS_PROBABLE_OBJECTS,
-        probability_reduction_factor=0.5)
-    camera_pose_sampler = suturo_blenderproc.sampler.pose_sampler.CameraPoseSampler(room=room)
+    camera_pose_sampler = CameraPoseSampler(room=room)
+    light_pose_sampler = LightPoseSampler(room=room)
+    object_pose_sampler = ObjectPoseSampler(furnitures=scene_collection.get("Shelves") + scene_collection.get("Tables"))
 
-    light_pose_sampler = suturo_blenderproc.sampler.pose_sampler.LightPoseSampler(room=room)
-
-    object_pose_sampler = suturo_blenderproc.sampler.pose_sampler.ObjectPoseSampler(
-        furnitures=scene_collection.get("Tables") + scene_collection.get("Shelves"))
     logger.log_component(iteration=0, component="Initialize")
     current_partition = 0
     surface = object_pose_sampler.get_current_surface()
@@ -65,7 +52,7 @@ def deploy_scene(x: int, scene_initializer: suturo_blenderproc.scene_init.SceneI
     partition_indices = []
 
     for i in range(x):
-        utils.blenderproc_utils.randomize_materials(furnitures)
+        # utils.blenderproc_utils.randomize_materials(furnitures)
         lights_strength = np.random.choice([10, 30, 40, 50, 100])
         radius = np.random.choice(np.linspace(start=1.6, stop=2.2, num=20))
         height = np.random.choice(np.linspace(start=1.4, stop=1.8, num=8))
@@ -75,7 +62,7 @@ def deploy_scene(x: int, scene_initializer: suturo_blenderproc.scene_init.SceneI
                 surfaces.append(object_pose_sampler.get_current_surface())
         light = light_pose_sampler.sample_light_for_furniture(surface, lights_strength)
         camera_pose_sampler.sample_circular_camera_poses_list(surfaces=[surface],
-                                                              num_poses=config.get_number_of_camera_samples(),
+                                                              num_poses=config.get_number_of_camera_poses(),
                                                               radius=radius,
                                                               height=height)
         for s in surfaces:
@@ -118,11 +105,10 @@ def deploy_scene(x: int, scene_initializer: suturo_blenderproc.scene_init.SceneI
 
 def pipeline():
     logger = Logger()
-    args = scenes.argparser.get_argparse()
-    config = utils.yaml_config.YAMLConfig(filename=args.config_yaml)
+    args = scripts.argparser.get_argparse()
+    config = suturo_blenderproc.utils.yaml_config.YAMLConfig(filename=args.config_yaml)
 
-    scene_initializer = suturo_blenderproc.scene_init.SceneInitializer(yaml_config=config,
-                                                                       path_id2name=config.get_id2name_path())
+    scene_initializer = suturo_blenderproc.scene_init.SceneInitializer(yaml_config=config)
 
     scene_initializer.initialize_scene()
     deploy_scene(config.get_number_of_iterations(), scene_initializer, logger)
